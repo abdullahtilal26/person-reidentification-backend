0a00c1bd4814ed9a1972045c5824933b959cab02
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-01-03 22:51:28 +0500

    get users route added in adminRoute

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addUser, mailCredentialsToUser } = require("../controller/adminController");
+const { addUser, mailCredentialsToUser, getUsers } = require("../controller/adminController");
 
 router.route("/adduser").post(addUser);
 router.route("/adduser/sendmail").post(mailCredentialsToUser);
+router.route("/getusers").get(getUsers);
 
 module.exports = router;
 // const { addEmailToRole } = require("../services/authRoleService");
