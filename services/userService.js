999917a11836e8e11e0656c51a97df46b885742c
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-01-03 22:37:39 +0500

    Email feature in user service

--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 const db = require("../models");
 const User = db.user;
 
@@ -11,10 +12,10 @@ const createUser = async (_user) => {
 
   const _userExist = await getUserByEmail(email);
   if (_userExist) return null;
-  
+
   const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
   const hashPassword = await bcrypt.hash(password, salt);
-  
+
   const userCreated = await User.create({
     email: email,
     name: name,
@@ -34,4 +35,52 @@ const getUserByEmail = async (_email) => {
   return user ? user : null;
 };
 
-module.exports = { createUser, getUserByEmail };
+const getUserPassword = async (email) => {
+  const user = await getUserByEmail(email);
+  if (!user) return null;
+  return user ? user.dataValues.password : null;
+};
+
+const sendEmail = async (
+  hostEmail,
+  hostPassword,
+  userEmail,
+  subject,
+  message
+) => {
+  return new Promise((resolve, reject) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: "hotmail",
+      auth: {
+        user: hostEmail, // generated ethereal user
+        pass: hostPassword, // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+    console.log("Admin Email: ", hostEmail);
+    console.log("Password: ", hostPassword);
+    // send mail with defined transport object
+    let information = transporter.sendMail(
+      {
+        from: hostEmail, // sender address
+        to: userEmail, // list of receivers
+        subject: subject, // Subject line
+        text: message, // plain text body
+      },
+      (err, info) => {
+        if (err) {
+          console.log("Handshake Error: ", err);
+          return reject({ status: false });
+        } else {
+          console.log(info.response);
+          return resolve({ status: true });
+        }
+      }
+    );
+  });
+};
+
+module.exports = { createUser, getUserByEmail, sendEmail };
