f59a327abecec517c982de952f54c3223eccf509
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-01-03 23:03:37 +0500

    DeleteUser route added in adminRoute

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { addUser, mailCredentialsToUser, getUsers } = require("../controller/adminController");
+const { addUser, mailCredentialsToUser, getUsers, deleteUser } = require("../controller/adminController");
 
 router.route("/adduser").post(addUser);
 router.route("/adduser/sendmail").post(mailCredentialsToUser);
 router.route("/getusers").get(getUsers);
+router.route("/deleteuser").delete(deleteUser);
 
 module.exports = router;
 // const { addEmailToRole } = require("../services/authRoleService");
