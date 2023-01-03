4f624f836755118a8beca6d7c96c15ff0b79ada7
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-01-03 22:35:31 +0500

    SendMail route added in adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addUser } = require("../controller/adminController");
+const { addUser, mailCredentialsToUser } = require("../controller/adminController");
 
 router.route("/adduser").post(addUser);
+router.route("/adduser/sendmail").post(mailCredentialsToUser);
 
 module.exports = router;
+// const { addEmailToRole } = require("../services/authRoleService");
+// router.route("/do").post(addEmailToRole);
