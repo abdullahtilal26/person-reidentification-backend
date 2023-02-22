72c4f9de565f263713753c44d920c84baed2746c
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-02-22 21:35:19 +0500

    Uncomment standby server port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app/app");
 
-const port = process.env.SERVER_PORT; //|| 8083;
+const port = process.env.SERVER_PORT || 8083;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
