84ff3ee3ece85e316e1899fbd756908d9199752e
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-03-12 21:35:39 +0500

    service methids added in directory service

--- services/directoryService.js
+++ services/directoryService.js
@@ -1,4 +1,5 @@
 const util = require("util");
+const path = require("path");
 const fs = require("fs");
 const fsa = require("fs").promises;
 const mkdir = util.promisify(fs.mkdir);
@@ -36,4 +37,66 @@ const deleteOnServer = async (dirPath) => {
   }
 };
 
-module.exports = { makeDirectory, deleteOnServer };
+const getVideosListFromDirectory = (folder) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(folder, (err, files) => {
+      if (err) reject(err);
+      else {
+        console.log("Files");
+        console.log(files);
+        var videos = files.filter((file) => {
+          return path.extname(file).toLowerCase() === ".mp4";
+        });
+        console.log(videos);
+        let _videos = videos.map((video) => {
+          let folderPathSplit = folder.split("\\");
+          let _folderName = folderPathSplit[folderPathSplit.length - 1];
+          let _fileName = path.join(
+            process.env.BASE_VIDEO_URI,
+            "upload",
+            _folderName,
+            video
+          );
+          return _fileName;
+        });
+        resolve(_videos);
+      }
+    });
+  });
+};
+
+const CopyVideosToFlaskServerDirectory = (sourceDir, targetDir) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(sourceDir, (err, files) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      for (let index = 0; index < files.length; index++) {
+
+        const sourceFilePath = path.join(sourceDir, files[index]);
+        const targetFilePath = path.join(targetDir, files[index]);
+
+        console.log(sourceFilePath)
+        console.log(targetFilePath)
+
+        fs.copyFile(sourceFilePath, targetFilePath, (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+        });
+      }
+
+      resolve(`Files Transfer successfully`);
+    });
+  });
+};
+
+module.exports = {
+  makeDirectory,
+  deleteOnServer,
+  getVideosListFromDirectory,
+  CopyVideosToFlaskServerDirectory,
+};
