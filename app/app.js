b60913cb52b2fd46e892c1f3bafdb825b7b8096d
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-03-12 21:31:43 +0500

    Public path added in app.js

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const app = express();
+const path = require("path");
 const db = require("../models/index");
 
 // dependencies
@@ -13,10 +15,10 @@ const userRoute = require("../routes/userRoutes");
 // middleware expression
 const { validateToken } = require("../middleware/JWT");
 const { validRole } = require("../middleware/validRole");
-const app = express();
 
 // middleware
 app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(express.static(path.join(process.env.PUBLIC_PATH)));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -38,5 +40,6 @@ db.sequelize
 app.use("/user", validateToken, userRoute);
 app.use("/auth", authRoute);
 app.use("/admin", validateToken, validRole, adminRoute);
+app.use("*", (req, res) => res.send("Error - No Page Found"));
 
 module.exports = app;
