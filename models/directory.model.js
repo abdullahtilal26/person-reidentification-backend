04ec744fe4bdba526e76bdacf2693e75cff17af0
salman-ahmed979 <salman.ahme979@hotmail.com>
2023-03-12 21:35:11 +0500

    auto id added in directory model

--- models/directory.model.js
+++ models/directory.model.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Directories = sequelize.define("directories", {
+    dir_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     dirpath: {
       type: DataTypes.STRING,
       primaryKey: true,
